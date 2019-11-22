db435ad585ba09ec04383ad4dbe8c217147d8260
nguyentungduc <nguyentungducbk@gmail.com>
2019-11-22 23:25:30 +0700

    cut layout admin

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,40 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function(){
+    return view('public.home');
+})->where('any', '.*');
 
-Route::get('/', function () {
-    return view('welcome');
+//====admin=====
+Route::prefix('admin')->group(function () {
+
+    //login
+    Route::get('login', function () {
+        return view('admin.auth.login', ['msg' => null]);
+    });
+
+    //dashboard
+    Route::get('dashboard', function () {
+        return view('admin.dashboard', ['msg' => null]);
+    });
+
+    //user
+    Route::prefix('profile')->group(function(){
+        Route::get('view', function () {
+            return view('admin.profile.view', ['msg' => null]);
+        });
+        Route::get('change-password', function (){
+            return view('admin.profile.change-password', ['msg' => null]);
+        });
+    });
+
+    //staticpage
+    Route::prefix('static-pages')->group(function(){
+        Route::get('all', function () {
+            return view('admin.staticpage.all', ['msg' => null]);
+        });
+        Route::get('new', function (){
+            return view('admin.staticpage.new', ['msg' => null]);
+        });
+    });
+
 });
